9f52832fe79f0a805d84c270145e3bf6fec4ff5a
tangs <tangsong1988kaka@gmail.com>
2020-02-28 18:47:06 +0800

    client read response.

--- src/client.c
+++ src/client.c
@@ -2,8 +2,26 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+uv_stream_t* stream;
+
+void alloc_cb(uv_handle_t* handle, size_t suggested_size, uv_buf_t* buf) {
+  buf->base = malloc(suggested_size);
+  buf->len = suggested_size;
+}
+
+void read_cb(uv_stream_t* stream, ssize_t nread, const uv_buf_t* buf) {
+  if (nread == UV_EOF) {
+    printf("read end.\n");
+    return;
+  } else if (nread < 0) {
+    fprintf(stderr, "read err:%zd\n", nread);
+  }
+  printf("read:%s\n", buf->base);
+}
+
 void cb(uv_write_t* req, int status) {
   printf("req, status:%d\n", status);
+  uv_read_start(stream, alloc_cb, read_cb);
   // uv_close(req);
 }
 
@@ -11,11 +29,13 @@ void on_connect(uv_connect_t* req, int status) {
   printf("status:%d\n", status);
   if (status < 0) return;
   uv_write_t req1;
+  char* str = "Hello libuv.\n";
   uv_buf_t a[] = {
-    { .base = "1", .len = 1 },
-    { .base = "2", .len = 1 }
+    { .base = str, .len = strlen(str) },
   };
-  uv_write(&req1, req->handle, a, 2, cb);
+  stream = req->handle;
+  // uv_write(&req1, req->handle, a, 2, cb);
+  uv_write(&req1, stream, a, 1, cb);
 }
 
 int main(int argc, char** argv) {
